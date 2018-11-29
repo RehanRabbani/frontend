23696146e7948b60a2a832f6a841125950c94f5f
RehanRabbani <rabbanirehan6@gmail.com>
2018-11-29 19:08:54 +0500

    header compelete

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,61 @@ import React from 'react';
 import './../index.css'
 
 class Header extends React.Component{
+    constructor(props){
+        super(props);
+        this.myFunction =this.myFunction.bind(this);
+    }
+   myFunction () {
+        let x = document.getElementById("myTopnav");
+        if (x.className === "StyleHead") {
+            x.className += " MobileHaeder"
+        } else {
+            x.className = "StyleHead"
+        }
+        console.log("helo");
+    }
     render()
     {
         return (
-           <div className= "StyleHead">
-             
-           <p>About</p>
-           <p>Home</p>
-           <p>CMS</p>
             
-           <input type="search" placeholder="search" />
-           <button>login</button>
-           <a href="#"> Become Seller</a>
-           </div>
+           
+                     <div className= "StyleHead" id="myTopnav">
+                     
+                     
+
+                        <div className="UpperHeader">
+                        <p>About</p>
+                        <p>Home</p>
+                        <p>CMS</p>
+                        
+                         </div>
+                   
+                        <div className="LowerHeader">
+                        <div onClick={this.myFunction}><i class="fas fa-bars"></i></div>
+                             <div className="BrandName">
+                      
+                             <p>Sbay</p>
+                            </div>
+                            
+                            <div className="input-group mb-3 show">
+                                 <input type="text" class="form-control" placeholder="search" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                                 <div class="input-group-append">
+                                 <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
+                                 </div>
+                            </div> 
+                           <div> <button type="button" class="btn btn-outline-primary">Login</button></div>
+                          <div>  <button type="button" class="btn btn-link">Become Seller</button></div>
+                         
+                         </div>
+                         
+                    
+
+                         
+                    </div>
+                    
+                    
+                    
+            
         )
     }
 }
